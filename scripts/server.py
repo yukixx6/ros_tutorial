eacca7c61f44f85f072279e88cd8d1e392aa6dd3
yukixx6 <s1526707DA@s.chibakoudai.jp>
2019-02-11 20:25:37 +0900

    Update server.py

--- scripts/server.py
+++ scripts/server.py
@@ -4,15 +4,17 @@ from std_srvs.srv import SetBool, SetBoolResponse
 
 def callback_srv(data):
     resp = SetBoolResponse()
-    resp.success = data.data
     if data.data == True:
         resp.message = "called"
+        resp.success = True
     else:
         resp.message = "ready"
+        resp.success = False
     print(resp.message)
     return resp
 
 if __name__ == "__main__":
     rospy.init_node("srv_server")
     srv = rospy.Service('service_call', SetBool, callback_srv)
+    print("ready")
     rospy.spin()
